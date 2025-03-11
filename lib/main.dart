7fdf562af4597db4e3b516533aa7d739faadd795
theabdulraffay <abdulraffay2494@gmail.com>
2025-03-12 00:00:47 +0500

    feat: integrate NewsBloc into HomeScreen and implement news fetching logic

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:news_app/bloc/news_bloc/news_bloc.dart';
 import 'package:news_app/bloc/signup_bloc/sign_up_bloc.dart';
 import 'package:news_app/firebase_options.dart';
 import 'package:news_app/screens/splash_screen.dart';
@@ -20,7 +21,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => SignUpBloc())],
+      providers: [
+        BlocProvider(create: (context) => SignUpBloc()),
+        BlocProvider(create: (context) => NewsBloc()),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/bloc/news_bloc/news_bloc.dart';
+import 'package:news_app/bloc/news_bloc/states/news_states.dart';
 import 'package:news_app/bloc/signup_bloc/sign_up_bloc.dart';
-import 'package:news_app/screens/sign_up_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,11 +15,50 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+    context.read<NewsBloc>().add(FetchNewsEvent());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: BlocBuilder<NewsBloc, NewsState>(
+        builder: (context, state) {
+          if (state.states is NewsLoadingState ||
+              state.states is NewsLoadingState) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state.states is NewsSuccessState) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(child: Text('This is Home Screen')),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.news!.articles!.length,
+                    itemBuilder: (context, index) {
+                      final article = state.news!.articles![index];
+                      return ListTile(
+                        title: Text(article.title ?? ' '),
+                        subtitle: Text(article.description ?? ' '),
+                      );
+                    },
+                  ),
+                ),
+
+                TextButton(
+                  onPressed: () {
+                    BlocProvider.of<SignUpBloc>(context).add(SignOut());
+                    // context.read<SignUpBloc>().add(SignOut());
+                  },
+                  child: Text('Logout'),
+                ),
+              ],
+            );
+          } else {
+            return Center(child: Text('Failed to load news'));
+          }
+        },
+      ),
+    );
   }
 }
 
